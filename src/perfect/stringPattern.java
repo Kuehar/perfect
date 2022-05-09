941b60e62b75c8445bd0b8f9922429458bc04008
kuehar <green95251@gmail.com>
2022-05-09 21:57:47 +0900

    added inefficient concat method to stringPattern.

--- src/perfect/stringPattern.java
+++ src/perfect/stringPattern.java
@@ -11,4 +11,14 @@ public class stringPattern {
 		StringBuilder sb2 = new StringBuilder("abc");
 		String s2 = sb2.toString();
 	}
+	
+	// p.32
+	// it might be ineffective concat.
+	static String concat(String[] array) {
+		String result = "";
+		for(String s: array) {
+			result += s; // This line create StringBuilder object and call append function twice in each loop. 
+		}
+		return result;
+	}
 }
