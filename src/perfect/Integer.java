8f2a1be3ed03499d4734a06d46226c689e41c3e0
kuehar <green95251@gmail.com>
2022-05-31 21:23:20 +0900

    replaced integerType to main to debug.

--- src/perfect/Integer.java
+++ src/perfect/Integer.java
@@ -3,11 +3,11 @@ package perfect;
 public class Integer {
 	
 	// P.49
-	public void integerType() {
+	public static void main(String[] args) {
 		int i = 10;
 		System.out.println(i); // => 10
 		
 		i = i*2;
-		System.out.println(i);　// => 20
+		System.out.println(i); // => 20	
 	}
 }
