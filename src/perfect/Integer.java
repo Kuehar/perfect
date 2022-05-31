8e120a827411fe82d189900d407e8b13f3dc8e61
kuehar <green95251@gmail.com>
2022-05-31 21:08:33 +0900

    created Integer.java

---
+++ src/perfect/Integer.java
@@ -0,0 +1,13 @@
+package perfect;
+
+public class Integer {
+	
+	// P.49
+	public void integerType() {
+		int i = 10;
+		System.out.println(i); // => 10
+		
+		i = i*2;
+		System.out.println(i);　// => 20
+	}
+}
